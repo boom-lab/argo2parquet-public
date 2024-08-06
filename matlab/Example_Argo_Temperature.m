cf0ca6370209cff4c7798df34aa267e3525dc60c
Enrico <enrico.milanese@whoi.edu>
2024-08-06 14:11:08 -0400

    added plot to matlab example

--- matlab/Example_Argo_Temperature.m
+++ matlab/Example_Argo_Temperature.m
@@ -1,10 +1,13 @@
-%%% Plotting temperature measurements in the North-West Atlantic
+%% ===================================================================== %%
+% Plotting temperature measurements in the North-West Atlantic
+% ======================================================================= %
 %
 % This examples shows how to read and manipulate Argo data stored in
 % parquet format. We will filter the data by depth (pressure), time, and
 % geographical location. We will then compute the average value of the
 % temperature for each float, and plot it.
 
+%% Setup
 % Clear all variables and add relative path
 clear all
 addpath( fullfile('.','tools') );
@@ -23,6 +26,7 @@ selectVariables = {...
     'LATITUDE',...
     'LONGITUDE',...
     'JULD',...
+    'PLATFORM_NUMBER',...
     'PRES_ADJUSTED',...
     'TEMP_ADJUSTED'...
     };
@@ -34,6 +38,7 @@ selectVariables = {...
 % in which case all the parameters are read.
 pds = setUpReader(targetDatabase, selectVariables);
 
+%% Filtering
 % We now create the filters for our data,. The syntax is:
 % filterby(varName, minValue [, maxValue, filter])
 % such that: minValue <= varName <= maxValue
@@ -53,26 +58,47 @@ pds = setUpReader(targetDatabase, selectVariables);
 filter = filterBy('PRES_ADJUSTED',50,[]);
 
 % Filtering geographical coordinates for NWA region
-% Note: we pass the filter we just generated to append this filter to it
+% Note: we pass the filter we just generated to append these filters to it
 filter = filterBy('LATITUDE',34,80,filter); 
-filter = filterBy('LONGITUDE',-78,-50,filter); 
+filter = filterBy('LONGITUDE',-78,-50,filter);
+
+% Filtering by date and time, e.g. if we want to look at the data between
+% 12pm on October 1st, 2023 and 9:15pm on April 30th, 2024
+startTime = datetime(2023,10,1,12,0,0); % year, month, day, hour (24h format), min, sec
+endTime   = datetime(2024,4,30,21,15,0); % year, month, day, hour (24h format), min, sec
+filter = filterBy('JULD',startTime,endTime,filter);
 
 % Applying filters to the database
 pds = applyFilter(pds,filter);
 
-% Reading the data into memory, serially (timing the operation)
-tic;
-dataSerial = readall(pds,UseParallel=false);
-elapsed = toc;
-disp("Elapsed time to read data into memory serially: " + num2str(elapsed) + " seconds.")
+%% Reading the data into memory, serially (timing the operation)
+% tic;
+% dataSerial = readall(pds,UseParallel=false);
+% elapsed = toc;
+% disp("Elapsed time to read data into memory serially: " + num2str(elapsed) + " seconds.")
 
-% Reading the data into memory, in parallel (timing the operation)
+%% Reading the data into memory, in parallel (timing the operation)
 p = gcp('nocreate');
 if isempty(p)
+    tic
     parpool; % you can specificy the number of workers with 
              % parpool(nbWorkers); the default shoud be fine
+    elapsed = toc;
+    disp("Elapsed time to  create parallel environment: " + num2str(elapsed) + " seconds.")
 end
 tic;
 dataParallel = readall(pds,UseParallel=true);
 elapsed = toc;
 disp("Elapsed time to read data into memory in parallel: " + num2str(elapsed) + " seconds.")
+
+%% Plotting target data
+% Now we can make a scatter plot of the temperature data recorded
+scatterMap(dataParallel, 'TEMP_ADJUSTED', 'Temperature in NWA at 50 dbar')
+
+%% Basic statistics
+% We can also quickly investigate some statistics of the loaded data
+dataStats = summary(dataParallel);
+% and for example see the min, max, and median values of the temperatue
+disp("Minimum temperature = " + num2str(dataStats.TEMP_ADJUSTED.Min));
+disp("Maximum temperature = " + num2str(dataStats.TEMP_ADJUSTED.Max));
+disp("Median temperature = " + num2str(dataStats.TEMP_ADJUSTED.Median));
